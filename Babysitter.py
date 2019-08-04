0cbabae2076d7f3be5053a2c97c8d34d61e1e3f2
Jitendra Godha <jjgodha@yahoo.com>
2019-08-04 18:38:38 -0400

    Implementation for calculating charges for shift going beyond midnight for family A

--- Babysitter.py
+++ Babysitter.py
@@ -3,7 +3,13 @@ class Babysitter:
         pass
 
     def calculate(self, start, end):
-        charges = (end-start) * 15
-        if end > 23:
-            charges += (end - 23) * 5
+        charges = 0
+        if end <= 24 and end >= 18:
+            charges = (end-start) * 15
+        if end == 24:
+            charges += 5
+        if end in (1, 2, 3, 4):
+            charges = ((24 - start) * 15) + 5
+            for hour in range(0, end):
+                charges += 20
         return charges
