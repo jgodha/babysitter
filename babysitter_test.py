948af9d3f7edb8be9f7417dc0d1c4dec99a8313d
Jitendra Godha <jjgodha@yahoo.com>
2019-08-03 23:10:05 -0400

    Refactoring to eliminate class setup duplication

--- babysitter_test.py
+++ babysitter_test.py
@@ -3,13 +3,14 @@ from Babysitter import Babysitter
 
 class BabysitterTest(unittest.TestCase):
 
+    def setUp(self):
+        self.babysitter = Babysitter()
+
     def test_class_creation(self):
-        babysitter = Babysitter()
-        self.assertNotEqual(babysitter, None)
+        self.assertNotEqual(self.babysitter, None)
 
     def test_charges_for_familyA_without_rate_change(self):
-        babysitter = Babysitter()
-        self.assertEqual(babysitter.calculate(17, 22), 75)
+        self.assertEqual(self.babysitter.calculate(17, 22), 75)
 
 if __name__ == '__main__':
     unittest.main()
