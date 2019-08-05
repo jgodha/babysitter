c973b551167a7f8f117652a5924ae14600fbd12c
Jitendra Godha <jjgodha@yahoo.com>
2019-08-05 17:41:29 -0400

    Failing test for calculating charges with rate change before midnight for family B

--- babysitter_test.py
+++ babysitter_test.py
@@ -1,5 +1,5 @@
 import unittest
-from Babysitter import Babysitter
+from babysitter import Babysitter
 
 class BabysitterTest(unittest.TestCase):
 
@@ -9,17 +9,20 @@ class BabysitterTest(unittest.TestCase):
     def test_class_creation(self):
         self.assertNotEqual(self.babysitter, None)
 
-    def test_charges_for_familyA_without_rate_change(self):
+    def test_charges_for_family_A_without_rate_change(self):
         self.assertEqual(self.babysitter.calculate(17, 22, "A"), 75)
 
-    def test_charges_for_familyA_with_rate_change_before_midnight(self):
+    def test_charges_for_family_A_with_rate_change_before_midnight(self):
         self.assertEqual(self.babysitter.calculate(17, 24, "A"), 110)
 
-    def test_charges_for_familyA_with_shift_going_beyond_midnight(self):
+    def test_charges_for_family_A_with_shift_going_beyond_midnight(self):
         self.assertEqual(self.babysitter.calculate(17, 1, "A"), 130)
 
-    def test_charges_for_familyB_without_rate_change(self):
+    def test_charges_for_family_B_without_rate_change(self):
         self.assertEqual(self.babysitter.calculate(17, 21, "B"), 48)
+
+    def test_charges_for_family_B_with_rate_change_before_midnight(self):
+        self.assertEqual(self.babysitter.calculate(18, 24, "B"), 64)
 
 if __name__ == '__main__':
     unittest.main()
